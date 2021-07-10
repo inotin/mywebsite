a8287fa383603f37cd4308f78a1e5a0ca5825895
inotin <notin.i@ya.ru>
2021-07-10 11:00:28 +0200

    Bug fixed in PGN project

--- ds/pgn.py
+++ ds/pgn.py
@@ -12,15 +12,21 @@ from traitlets.config import Config
 from nbconvert import HTMLExporter
 from urllib.request import urlopen
 import nbformat
+from bs4 import BeautifulSoup
+import requests
+import re
 
 
 def convertPGNtoDF(path):
     """"""
-    with open(path, 'r') as file:
-        data_from_pgn = file.read()
+    try:
+        with open(path, 'r') as file:
+            data_from_pgn = file.read()
+    except:
+        with open(path, 'r', encoding='windows-1252') as file:
+            data_from_pgn = file.read()
 
     dct = defaultdict(list)
-
     moves = False
     for element in data_from_pgn.split("\n\n"):
         tags=['Event', 'Site', 'Date', 'Round', 'White', 'Black', 'Result', 'WhiteElo', 'BlackElo', 'ECO']
@@ -39,70 +45,114 @@ def convertPGNtoDF(path):
                 dct[t].append(np.nan)
                 #print("nan for ", tag)
             moves = True
-
+    #filtering only ECO
+    dct = {key: dct[key] for key in ["ECO", "Event"] }
     return pd.DataFrame(dct)
 
 def getECOdesc(eco):
     try:
-        return ecoDct[eco][0]
+        ecoDf = pd.read_html('https://www.chessgames.com/chessecohelp.html')
+        ecoDf=ecoDf[0]
+        ecoDf.columns = ['ECO','desc']
+        return ecoDf[ecoDf['ECO']==eco]['desc'].values[0]
     except:
         return np.nan
+
+def getEcoDescription(eco, ecoUrl = 'https://www.chessgames.com/chessecohelp.html'):
+    """
+    The function returns a description of an opening based on its ECO code
+
+    Input:
+    eco (str): ECO code, e.g. 'A08'
+    ecoUrl(str): source of the table with descriptions,
+                 default: 'https://www.chessgames.com/chessecohelp.html'
+
+    Output (str): string with a description or 'Description was not found' in case of abscence
+    """
+
+    pathToEco = './downloads/eco.pkl'
+    if os.path.isfile(pathToEco):
+        ecoDf = pd.read_pickle(pathToEco)
+    else:
+        ecoDf = pd.read_html(ecoUrl)[0]
+        ecoDf.to_pickle(pathToEco)
+    try:
+        result = ecoDf[ecoDf[0]==eco].iloc[0][1]
+    except:
+        result = 'Description was not found'
+    return result
+
+
+def playerExists(playerName, url="https://www.pgnmentor.com/files.html"):
+    session = requests.session()
+    response = session.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    lst = []
+    for element in soup.find_all('a', href=re.compile('^players')):
+        el = element['href'].split('/')[1]
+        if '.zip' not in el:
+            lst.append(el.lower())
+    return playerName.lower() in set(lst)
 
 def getGraph(lastname, top = 30, directory_to_extract_to = 'downloads'):
     """
     """
-    #print('Downloading ZIP with PGNs of '+lastname)
-    zipFileUrl = "https://www.pgnmentor.com/players/"+lastname+".zip"
-    zipFile = requests.get(zipFileUrl)
+    if playerExists(lastname):
+        #print('Downloading ZIP with PGNs of '+lastname)
+        zipFileUrl = "https://www.pgnmentor.com/players/"+lastname+".zip"
+        zipFile = requests.get(zipFileUrl)
 
-    if not os.path.exists(directory_to_extract_to):
-        os.makedirs(directory_to_extract_to)
+        if not os.path.exists(directory_to_extract_to):
+            os.makedirs(directory_to_extract_to)
+        else:
+            shutil.rmtree(directory_to_extract_to)           # Removes all the subdirectories!
+            os.makedirs(directory_to_extract_to)
+
+        path_to_zip_file = "downloads/"+lastname+'.zip'
+
+        #print("Download successful")
+        with open(os.fspath(PurePath(path_to_zip_file)), 'wb') as f:
+            f.write(zipFile.content)
+
+        zip_ref = zipfile.ZipFile(os.fspath(PurePath(path_to_zip_file)), 'r')
+        zip_ref.extractall(os.fspath(PurePath(directory_to_extract_to)))
+        zip_ref.close()
+
+        rawDf = convertPGNtoDF(directory_to_extract_to+'/'+lastname+'.pgn')
+
+        df = rawDf.groupby("ECO").count().sort_values(by="Event", ascending=False).head(top)
+        df.reset_index(inplace=True)
+        df.drop(df.iloc[:, 2:], axis = 1, inplace = True)
+        df.columns=["ECO","Frequency"]
+
+        dr = requests.get("http://www.neerpeltersk.be/palview4/p4sets/P3eco.txt")
+        path_to_Eco = 'static/eco.txt'
+
+        with open(path_to_Eco, 'wb') as f:
+            f.write(dr.content)
+
+        with open(path_to_Eco, 'r') as file:
+            data_from_eco = file.read()
+
+        ecoDct = defaultdict(list)
+        for row in data_from_eco.split("\n\n")[4:]:
+            code = row[:3]
+            desc = row[3:].lstrip()
+            ecoDct[code].append(desc)
+
+        df["desc"]=df["ECO"].map(getECOdesc)
+        df["perc"]=df["Frequency"]/len(rawDf)
+        fig = px.bar(df, x='ECO', y='Frequency',
+                 hover_data=['desc','perc'],
+                 height=400,
+                 title = "Top-"+str(top)+" openings of "+lastname,
+                 labels={'ECO':'ECO (opening code)',
+                         'Frequency':'Frequency',
+                        'perc':'Ratio'},
+                 color='Frequency',
+                 color_continuous_scale="rdbu_r"
+                )
+        fig.write_html("static/graph.html")
     else:
-        shutil.rmtree(directory_to_extract_to)           # Removes all the subdirectories!
-        os.makedirs(directory_to_extract_to)
-
-    path_to_zip_file = "downloads/"+lastname+'.zip'
-
-    #print("Download successful")
-    with open(os.fspath(PurePath(path_to_zip_file)), 'wb') as f:
-        f.write(zipFile.content)
-
-    zip_ref = zipfile.ZipFile(os.fspath(PurePath(path_to_zip_file)), 'r')
-    zip_ref.extractall(os.fspath(PurePath(directory_to_extract_to)))
-    zip_ref.close()
-
-    rawDf = convertPGNtoDF(directory_to_extract_to+'/'+lastname+'.pgn')
-
-    df = rawDf.groupby("ECO").count().sort_values(by="Event", ascending=False).head(top)
-    df.reset_index(inplace=True)
-    df.drop(df.iloc[:, 2:], axis = 1, inplace = True)
-    df.columns=["ECO","Frequency"]
-
-    dr = requests.get("http://www.neerpeltersk.be/palview4/p4sets/P3eco.txt")
-    path_to_Eco = 'static/eco.txt'
-
-    with open(path_to_Eco, 'wb') as f:
-        f.write(dr.content)
-
-    with open(path_to_Eco, 'r') as file:
-        data_from_eco = file.read()
-
-    ecoDct = defaultdict(list)
-    for row in data_from_eco.split("\n\n")[4:]:
-        code = row[:3]
-        desc = row[3:].lstrip()
-        ecoDct[code].append(desc)
-
-    df["desc"]=df["ECO"].map(getECOdesc)
-    df["perc"]=df["Frequency"]/len(rawDf)
-    fig = px.bar(df, x='ECO', y='Frequency',
-             hover_data=['desc','perc'],
-             height=400,
-             title = "Top-"+str(top)+" openings of "+lastname,
-             labels={'ECO':'ECO (opening code)',
-                     'Frequency':'Frequency',
-                    'perc':'Ratio'},
-             color='Frequency',
-             color_continuous_scale="rdbu_r"
-            )
-    fig.write_html("static/graph.html")
+        with open("static/graph.html", "w") as file:
+            file.write(f"<h4 style='margin-left:30px; margin-top:15px'> Player {lastname} was not found in <a target='_blank' href='https://www.pgnmentor.com/files.html'>the database</a></h4>")
